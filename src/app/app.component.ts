fae5df239dda5f4eaa240e8505fab7f2f5d62d86
Matias Navio <naviomatias2@gmail.com>
2024-05-21 19:56:54 -0300

    Directivas for e if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-app';
+  name: String = ' Hola Matias Navio';
+
+  enabled: boolean = false;
+
+  courses: string[] = ['Angular', 'React', 'Spring'];
+
+  setEnable(): void {
+    this.enabled = this.enabled ? false : true;
+  }
 }
